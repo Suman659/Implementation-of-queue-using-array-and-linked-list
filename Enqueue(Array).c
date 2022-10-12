e3487c2a6fc5d5ebe0659a15d35eae43971b3a4a
Suman Mahato <103480485+Suman659@users.noreply.github.com>
2022-10-12 14:47:53 +0530

    Update Enqueue(Array).c

--- Enqueue(Array).c
+++ Enqueue(Array).c
@@ -1,1 +1,11 @@
-
+void enqueue(int value){
+  if((rear+1)%size==front);
+  printf("Queue is full.\n");
+  else{
+    if(front==-1)
+      front=0;
+    
+    rear=(rear+1)%size;
+    queue[rear]=value;
+  }
+}
