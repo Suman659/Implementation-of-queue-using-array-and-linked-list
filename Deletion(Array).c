ded25133ac52f565244511d4c71ca6a40f40ffb7
Suman Mahato <103480485+Suman659@users.noreply.github.com>
2022-10-12 14:57:01 +0530

    Update Deletion(Array).c

--- Deletion(Array).c
+++ Deletion(Array).c
@@ -1,1 +1,15 @@
-
+int dequeue(){
+  intx;
+   if(isempty())
+     printf("Queue is empty\n";
+  else if(front==rear){
+x=queue[front];
+front=-1;
+rear=-1;
+}
+else{
+  x=queue[front];
+  front=(front+1)%SIZE;
+}
+return x;
+}
